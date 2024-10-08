d30c9327e8bf01abfc0ac112922b36f7c9ad271d
roy5820 <35182721+roy5820@users.noreply.github.com>
2024-10-08 16:48:34 +0900

    메뉴를 통한 로비 접속#1

--- Assets/Script/GameSystem/GameManager.cs
+++ Assets/Script/GameSystem/GameManager.cs
@@ -1,11 +1,13 @@
 using AshGreen.Singleton;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AshGreen
 {
     public class GameManager : Singleton<GameManager>
     {
-        
+
     }
 }
