0c29b8399b3b415ca29474145fa56995f826f4de
roy5820 <35182721+roy5820@users.noreply.github.com>
2024-10-29 12:59:38 +0900

    세션 종료 처리 수정

--- Assets/Script/UI/BtnController.cs
+++ Assets/Script/UI/BtnController.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.UIElements;
 namespace AshGreen.UI
 {
     public class BtnController : MonoBehaviour
     {
+        private ulong clientId;
+
+        private void Start()
+        {
+            clientId = NetworkManager.Singleton.LocalClientId;
+        }
+
         public void OnShutdown()
         {
-            ClientConnection.Instance.RemoveClient(NetworkManager.Singleton.LocalClientId);
+            
+            ClientConnection.Instance.RemoveClient(clientId);
         }
     } 
 }
